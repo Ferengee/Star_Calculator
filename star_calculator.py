67bae567e41b5d9b94d483737fafbcccbd3452c1
codenamecreps <creps7055@gmail.com>
2013-08-12 19:14:39 -0700

    Update star_calculator.py
    An attempt to document how long it takes light to travel to the closest 10 stars to our solar system (the sun included).

--- star_calculator.py
+++ star_calculator.py
@@ -1,2 +1,53 @@
 #star_calculator
 
+sol = 299792458.0 
+#speed of light (meter/second)
+
+meter = 3.28084 
+#feet
+
+#Time 
+#---------------------
+year = 365.0
+#days
+
+hour = 24.0
+#hours in a day
+
+minute = 60.0
+#seconds
+
+second = 60.0 
+#seconds in a minute
+
+mid = hour * minute
+#minutes in day
+
+sid = hour * minute * second
+#seconds in day
+
+yis = year * sid
+#year in seconds
+
+yim = year * mid
+#year in minutes
+
+#Distance
+#----------------------
+dlpy = sol * yis
+#distance of light per year (meters)
+
+ltts = 8.3
+#light's time to sun (minutes)
+
+dts = sol * ltts * second
+#distance to sun (meters)
+
+print "The speed of light is %r meters/second." % sol
+print "There are %r seconds in a year." % yis
+print "Which equals %r minutes per year." % yim
+print "Light travels %r meters per year." % dlpy
+print "Can you belive that?"
+print "It takes light only %r minutes to get from the sun to earth" % ltts
+print "That means light travels %r meters in %r minutes" % (dts, ltts)
+
